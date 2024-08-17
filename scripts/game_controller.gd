1d0a0c024b2c62e0f35cb42a80e7050e96771b6a
dasei <32671526+dasei@users.noreply.github.com>
2024-08-17 18:52:43 +0200

    player moves

--- scripts/game_controller.gd
+++ scripts/game_controller.gd
@@ -4,7 +4,9 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Camera2D.position = $Dungeon.grid_to_world_pos($Dungeon.max_dungeon_size / 2)
+	$Player.position = $Dungeon.grid_to_world_pos($Dungeon.max_dungeon_size / 2)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	$Camera2D.position = $Player.position
 	pass
